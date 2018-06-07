5d0d6e754a9c7141c848421b47b5ace88ec4accc
Administrator <Administrator@DESKTOP-7IJ3V0H>
2018-06-07 16:44:12 +0800

    添加项目文件。

---
+++ PostCheckInService/PostCheckInService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PostCheckInService
+{
+    public class PostCheckInService : IService.IService
+    {
+        private System.Timers.Timer timer = null;
+
+        public PostCheckInService()
+        {
+            timer = new System.Timers.Timer(2000);
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timer.Stop();
+            try
+            {
+                Console.WriteLine("提交签到数据");
+            }
+            catch (Exception ex)
+            {
+                SeatManage.SeatManageComm.WriteLog.Write(ex.Message);
+            }
+            finally
+            {
+                timer.Start();
+            }
+        }
+
+        public void Dispose()
+        {
+            timer.Stop();
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
---
+++ ScanLogSyncService/ScanLogSyncService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScanLogSyncService
+{
+    public class ScanLogSyncService : IService.IService
+    {
+        private System.Timers.Timer timer = null;
+        string intervalTime = System.Configuration.ConfigurationManager.AppSettings["GetDataInterval"];
+
+        public ScanLogSyncService()
+        {
+            timer = new System.Timers.Timer(int.Parse(intervalTime) * 1000);
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timer.Stop();
+            try
+            {
+                Console.WriteLine("执行扫码数据同步");
+            }
+            catch (Exception ex)
+            {
+                SeatManage.SeatManageComm.WriteLog.Write(ex.Message);
+            }
+            finally
+            {
+                timer.Start();
+            }
+        }
+
+        public void Dispose()
+        {
+            timer.Stop();
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+
+        public override string ToString()
+        {
+            return "扫码数据同步获取程序";
+        }
+    }
+}
